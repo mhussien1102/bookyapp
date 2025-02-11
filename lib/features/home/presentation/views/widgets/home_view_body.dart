883fbd0dabbdf256585dade623fc6c4b0a5ca6ab
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2025-02-11 15:47:24 +0200

    add first image in the list view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookyapp/core/utils/assets.dart';
 import 'package:bookyapp/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookyapp/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,7 +10,10 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [CustomAppBar()],
+      children: [
+        CustomAppBar(),
+        CustomListViewItem(),
+      ],
     );
   }
 }
