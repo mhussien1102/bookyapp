a372311d53830b034e725a706c7953e19b4dbc1f
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2025-02-11 16:27:16 +0200

    create style file

---
+++ lib/core/utils/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class Styles {
+  static const titleMedium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: Color(0xffFFFFFF),
+  );
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookyapp/core/utils/assets.dart';
+import 'package:bookyapp/core/utils/style.dart';
 import 'package:bookyapp/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookyapp/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookyapp/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,11 +11,23 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomAppBar(),
-        CustomListViewItem(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedListView(),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            textAlign: TextAlign.left,
+            "Best Seller",
+            style: Styles.titleMedium,
+          )
+        ],
+      ),
     );
   }
 }
