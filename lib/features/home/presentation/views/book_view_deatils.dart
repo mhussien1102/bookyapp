b681fbf8bbf65d54a988e636b7907d4544196b93
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2025-02-18 13:38:32 +0200

    create book deatils views screen

---
+++ lib/features/home/presentation/views/book_view_deatils.dart
@@ -0,0 +1,13 @@
+import 'package:bookyapp/features/home/presentation/views/widgets/book_view_details_body.dart';
+import 'package:flutter/material.dart';
+
+class BookViewDeatils extends StatelessWidget {
+  const BookViewDeatils({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BookViewDetailsBody(),
+    );
+  }
+}
