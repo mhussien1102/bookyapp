c11e814415337f61f6cefa34af15e5cb9f4de81e
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2025-02-21 18:11:32 +0200

    add custom book item

--- lib/features/home/presentation/views/widgets/book_view_details_body.dart
+++ lib/features/home/presentation/views/widgets/book_view_details_body.dart
@@ -1,15 +1,25 @@
 import 'package:bookyapp/features/home/presentation/views/widgets/customBookDeatilsAppBar.dart';
 import 'package:flutter/material.dart';
 
+import 'custom_list_view_item.dart';
+
 class BookViewDetailsBody extends StatelessWidget {
   const BookViewDetailsBody({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Column(
-        children: [CustomBookDeatialsAppBar()],
+        children: [
+          CustomBookDeatialsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: CustomListViewItem(),
+          )
+        ],
       ),
     );
   }
