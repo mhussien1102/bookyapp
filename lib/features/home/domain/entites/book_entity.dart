5129dc4938ae17bdd14619e93efa149ded9cbb01
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2025-03-02 23:03:25 +0200

    create the book entity

---
+++ lib/features/home/domain/entites/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity {
+  final String imagePath;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity(
+      this.imagePath, this.title, this.authorName, this.price, this.rating);
+}
