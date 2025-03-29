cc8e60285a7e99c51e7893b820ab94b12bacd700
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2025-03-29 21:14:24 +0200

    create book model

---
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -0,0 +1,66 @@
+import 'package:bookyapp/features/home/data/models/book_model/equb.dart';
+import 'package:bookyapp/features/home/data/models/book_model/saleInfo.dart';
+import 'package:bookyapp/features/home/data/models/book_model/searchInfo.dart';
+import 'package:bookyapp/features/home/data/models/book_model/volume_info.dart';
+
+import 'accessInfo.dart';
+
+class BookModel {
+  String? kind;
+  String? id;
+  String? etag;
+  String? selfLink;
+  VolumeInfo? volumeInfo;
+  SaleInfo? saleInfo;
+  AccessInfo? accessInfo;
+  SearchInfo? searchInfo;
+
+  BookModel(
+      {this.kind,
+      this.id,
+      this.etag,
+      this.selfLink,
+      this.volumeInfo,
+      this.saleInfo,
+      this.accessInfo,
+      this.searchInfo});
+
+  BookModel.fromJson(Map<String, dynamic> json) {
+    kind = json['kind'];
+    id = json['id'];
+    etag = json['etag'];
+    selfLink = json['selfLink'];
+    volumeInfo = json['volumeInfo'] != null
+        ? VolumeInfo.fromJson(json['volumeInfo'])
+        : null;
+    saleInfo =
+        json['saleInfo'] != null ? SaleInfo.fromJson(json['saleInfo']) : null;
+    accessInfo = json['accessInfo'] != null
+        ? AccessInfo.fromJson(json['accessInfo'])
+        : null;
+    searchInfo = json['searchInfo'] != null
+        ? SearchInfo.fromJson(json['searchInfo'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['kind'] = kind;
+    data['id'] = id;
+    data['etag'] = etag;
+    data['selfLink'] = selfLink;
+    if (volumeInfo != null) {
+      data['volumeInfo'] = volumeInfo!.toJson();
+    }
+    if (saleInfo != null) {
+      data['saleInfo'] = saleInfo!.toJson();
+    }
+    if (accessInfo != null) {
+      data['accessInfo'] = accessInfo!.toJson();
+    }
+    if (searchInfo != null) {
+      data['searchInfo'] = searchInfo!.toJson();
+    }
+    return data;
+  }
+}
