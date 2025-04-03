426fba4c895436bf80a1fb42548ec8d9b73322e1
mhussien1102 <143397704+mhussien1102@users.noreply.github.com>
2025-04-03 14:07:26 +0200

    fetch featured books home repo impl

---
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -0,0 +1,36 @@
+import 'package:bookyapp/core/errors/failure.dart';
+
+import 'package:bookyapp/features/home/domain/entites/book_entity.dart';
+
+import 'package:dartz/dartz.dart';
+
+import '../../domain/repo/home_repo.dart';
+import '../data_sources/home_local_data_source.dart';
+
+class HomeRepoImp extends HomeRepo {
+  final HomeRemoteDataSources homeRemoteDataSource;
+  final HomeLocalDataSources homeLocalDataSource;
+
+  HomeRepoImp(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      var bookList = homeLocalDataSource.fetchFeaturedBooks();
+      if (bookList.isNotEmpty) {
+        return right(bookList);
+      }
+      var books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
